8faeab530191e07a2831914837c0d31cd10f33c9
yamana <matsuokanana.tech@gmail.com>
2023-08-01 21:02:19 +0900

    リファクタリング：explanationsコントローラ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_quiz_histories, dependent: :destroy
 
-  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }  
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  def record_quiz_history(quiz_choice)
+    word_id = quiz_choice.quiz.word_id
+    is_correct = quiz_choice.is_correct
+    quiz_history = user_quiz_histories.create!(
+      word_id: word_id, 
+      quiz_choice: quiz_choice, 
+      is_correct: is_correct
+    )
+    increase_level if quiz_history.is_correct && user_quiz_histories.where(is_correct: true).count % 3 == 0
+    quiz_history
+  end
+
+  private
+
+  def increase_level
+    increment!(:level)
+  end
 end
