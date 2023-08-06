9ec0684960cf91dcc2ee8c99a2e2d66872a4654c
yamana <matsuokanana.tech@gmail.com>
2023-08-06 22:23:37 +0900

    conversationsコントローラの削除

--- app/controllers/conversations_controller.rb
+++
@@ -1,7 +0,0 @@
-class ConversaionsController < ApplicationController
-  def index
-    user = User.find(params[:user_id])
-    dialogues = Dialogue.where(level: user.level).order(:created_at).pluck(:content)
-    render json: { dialogues: dialogues }
-  end
-end
