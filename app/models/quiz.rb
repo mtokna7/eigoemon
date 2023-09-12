37044dcb521455abf0d5d566e4eecf7c664292c5
yamana <matsuokanana.tech@gmail.com>
2023-09-13 01:46:02 +0900

    誤答問題の復習機能を実装

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,12 @@
 class Quiz < ApplicationRecord
   belongs_to :word
   has_many :quiz_choices, dependent: :destroy
+
+  def correct_rate(user)
+    total = UserQuizHistory.where(user: user, word_id: self.word_id).count
+    correct = UserQuizHistory.where(user: user, word_id: self.word_id, is_correct: true).count
+    return 100.0 if total.zero?
+
+    ((correct.to_f / total) * 100).ceil
+  end
 end
