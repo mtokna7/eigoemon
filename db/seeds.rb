53ac3706b1a044baf48c7806c96084e51798d542
yamana <matsuokanana.tech@gmail.com>
2023-09-19 11:50:07 +0900

    タイムゾーンの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Conversation.create!(level:2, content:"（ ぼく ）英語えもん、「r」で始まる単語に書いてある、「ぅ」の ふりがなは 声をださなくていいよ！", speaker:0, order:1)
-Conversation.create!(level:2, content:"（ ぼく ）じゅうようなのは、口の形を「う」にしたあとに ラ行 の音を出すことなんだ！", speaker:0, order:2)
-Conversation.create!(level:2, content:"（えもん）がれ！", speaker:1, order:3)
-Conversation.create!(level:2, content:"（ ぼく ）・・・?", speaker:0, order:4)
-Conversation.create!(level:2, content:"（ ぼく ）たとえば「really」なんかは 「うぃーりー」 って言うとちゃんと認識されるんだよ。", speaker:0, order:5)
-Conversation.create!(level:2, content:"（えもん）がれ！", speaker:1, order:6)
-Conversation.create!(level:2, content:"（ ぼく ）・・・「がれ」ってなに？", speaker:0, order:7)
-Conversation.create!(level:2, content:"（えもん）「got it（わかった！）」のことダ。キミのまねをして ボクもふりがなをふってみたダ！", speaker:1, order:8)
-Conversation.create!(level:2, content:"（ ぼく ）（なんかちょっとクヤシイな・・・。）", speaker:0, order:9)
+# Conversation.create!(level:2, content:"（ ぼく ）英語えもん、「r」で始まる単語に書いてある、「ぅ」の ふりがなは 声をださなくていいよ！", speaker:0, order:1)
 
 
 
