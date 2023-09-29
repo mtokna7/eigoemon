6a734a9df76d817a5f3c5f0055101f3f45f8fd89
yamana <matsuokanana.tech@gmail.com>
2023-09-29 15:27:53 +0900

    rubocopに沿ったコードの修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,24 +1,26 @@
-class Admin::UsersController < ApplicationController
-  layout 'admin'
-  before_action :admin_user
+module Admin
+  class UsersController < ApplicationController
+    layout 'admin'
+    before_action :admin_user
 
-  def index
-    @users = User.all.order("created_at DESC")
-  end
+    def index
+      @users = User.all.order("created_at DESC")
+    end
 
-  def destroy
-  user = User.find(params[:id])
-  if user == current_user
-    redirect_to admin_users_path, alert: '自分自身を削除することはできません。'
-  else
-    user.destroy!
-    redirect_to admin_users_path, notice: '削除しました'
-  end
-end
+    def destroy
+      user = User.find(params[:id])
+      if user == current_user
+        redirect_to admin_users_path, alert: t('users.destroy.self_deletion_alert')
+      else
+        user.destroy!
+        redirect_to admin_users_path
+      end
+    end
 
-  private
+    private
 
-  def admin_user
-    redirect_to(root_path) unless current_user.admin?
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
   end
 end
--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,5 +1,7 @@
 class TutorialsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[quiz_show quiz_explanation library_index library_explanation autocomplete]
+  skip_before_action :authenticate_user!, only: %i[
+    quiz_show quiz_explanation library_index library_explanation autocomplete
+  ]
   before_action :set_quiz_sequence, only: %i[quiz_show quiz_explanation]
   before_action :next_quiz, only: %i[quiz_show]
   before_action :load_quiz, only: %i[quiz_show quiz_explanation]
