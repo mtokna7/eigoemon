97127d48b69417de56902dd6eec309d8a46b2758
yamana <matsuokanana.tech@gmail.com>
2023-11-17 01:29:01 +0900

    不正なクイズidが入力された場合の例外処理を追加

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,14 +4,20 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.find(params[:id])
     @quiz_choices = @quiz.quiz_choices
     handle_level_up
+  rescue ActiveRecord::RecordNotFound
+    redirect_to_next_quiz(alert: t("alert.next_quiz"))
   end
 
   def random_quiz
-    next_quiz = Quiz.next_quiz_for_user(current_user)
-    redirect_to quiz_path(next_quiz)
+    redirect_to_next_quiz
   end
 
   private
+
+  def redirect_to_next_quiz(alert: nil)
+    next_quiz = Quiz.next_quiz_for_user(current_user)
+    redirect_to quiz_path(next_quiz), alert: alert
+  end
 
   def handle_level_up
     if level_up_conditions_met?
